1733ebf2f1d3ed5481a8fa0468a9f18ab73e2418
Emanuele Acito <e.acito@myitplace.de>
2024-06-26 17:47:31 +0200

    PIOXD-244 OXID 7 - Removal of Payment Method Giropay
    - Improvements implemented as with OXID6 and ported to OXID7

--- Application/views/admin_twig/es/mollie_lang.php
+++ Application/views/admin_twig/es/mollie_lang.php
@@ -138,5 +138,5 @@ $aLang = array(
     'MOLLIE_PAYMENT_TYPE'                               => 'Payment type',
     'MOLLIE_TRANSACTION_ID'                             => 'Mollie Transaction ID',
     'MOLLIE_EXTERNAL_TRANSACTION_ID'                    => 'External Transaction ID',
-    'MOLLIE_PAYMENT_DISABLED_ACTIVATION'                => '<b style="color: red;"> Este método de pago Mollie ya no se puede activar, ya que se eliminará en breve!</b>',
+    'MOLLIE_PAYMENT_DISABLED_ACTIVATION'                => 'Este método de pago Mollie ya no se puede activar, ya que se eliminará en breve!',
 );
--- Application/views/admin_twig/it/mollie_lang.php
+++ Application/views/admin_twig/it/mollie_lang.php
@@ -138,5 +138,5 @@ $aLang = array(
     'MOLLIE_PAYMENT_TYPE'                               => 'Payment type',
     'MOLLIE_TRANSACTION_ID'                             => 'Mollie Transaction ID',
     'MOLLIE_EXTERNAL_TRANSACTION_ID'                    => 'External Transaction ID',
-    'MOLLIE_PAYMENT_DISABLED_ACTIVATION'                => '<b style="color: red;"> Questo metodo di pagamento Mollie non può più essere attivato perché sarà presto rimosso!</b>',
+    'MOLLIE_PAYMENT_DISABLED_ACTIVATION'                => 'Questo metodo di pagamento Mollie non può più essere attivato perché sarà presto rimosso!',
 );
