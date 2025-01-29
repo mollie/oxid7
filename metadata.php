c0551149a27ded9e9344934052ae113e51abb90b
Eric Vollrath <96vlocky96@gmail.com>
2025-01-29 16:17:22 +0100

    added latest Version to metadata.php amd composer.json

--- metadata.php
+++ metadata.php
@@ -88,7 +88,7 @@ ni costes de configuración. Solo va a pagar por las transacciones exitosas. Y s
 ",
     ],
     'thumbnail'    => 'img/mollie_logo.png',
-    'version'       => '1.1.3',
+    'version'       => '1.1.4',
     'author'        => 'Fatchip GmbH',
     'email'         => 'info@mollie.com',
     'url'          => 'https://www.mollie.com/',
